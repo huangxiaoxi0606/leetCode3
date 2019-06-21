8d33fc60c9d7f8f89f56a2b6e5efd2bd640c2e62
hhx03 <1007502956@qq.com>
2019-06-21 15:52:52 +0800

    棒球比赛

---
+++ calPoints/calPoints.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User : Hhx
+ * Date : 2019/6/21
+ * Time : 15:47
+ */
+class Solution {
+    /**
+     * @param String[] $ops
+     * @return Integer
+     */
+    function calPoints($ops) {
+        // $num = 0;$data=[];$sum =0;
+//         foreach($ops as $key=> $op){
+//             if($op == '+'){
+//                 // if($num >1){
+//                     array_push($data,$data[$num-2] +$data[$num-1]);
+//                     $sum = $sum + $data[$num-2] +$data[$num-1];
+//                     // $num ++;
+//                 // }
+//             }elseif($op == 'D'){
+//                 // if($num>0){
+//                     array_push($data,$data[$num-1]*2);
+//                     $sum = $sum +$data[$num-1]*2;
+//                     $num ++;
+//                 // }
+//             }elseif($op == 'C'){
+//                 // if($num>0){
+//                     $sum = $sum - array_pop($data);
+//                     $num --;
+//                 // }
+//             }else{
+//                 array_push($data,$op);
+//                 $num ++;
+//                 $sum = $sum + $data[$num]+$op;
+//             }
+//         }
+//        return $sum;
+    }
+}
