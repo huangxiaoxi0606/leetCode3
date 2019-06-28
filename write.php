50dbfe5db3d67e917a9944ddfca0f441d139b6af
hhx03 <1007502956@qq.com>
2019-06-28 11:09:06 +0800

    findOcurrences

--- write.php
+++ write.php
@@ -5,8 +5,8 @@
  * Date : 2019/6/3
  * Time : 15:36
  */
-$chinaName ='单调数列';
-$englishName = "isMonotonic";
-$no = 896;
+$chinaName ='Bigram 分词';
+$englishName = "findOcurrences";
+$no = 1078;
 $str = $chinaName.'|'.$no.'|[' .$englishName.' url](https://github.com/huangxiaoxi0606/leetCode3/blob/master/'.$englishName.'/'.$englishName.'.php) | ['.$englishName.' url_test](https://github.com/huangxiaoxi0606/leetCode3/blob/master/'.$englishName.'/test.php)';
 file_put_contents('./leetCode.md', PHP_EOL.$str, FILE_APPEND);
