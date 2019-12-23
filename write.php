6ab2162f0e0cafbdae8e397779469937609c92c4
hhx <1007502956@qq.com>
2019-12-23 15:36:17 +0800

    排列硬币

--- write.php
+++ write.php
@@ -5,8 +5,8 @@
  * Date : 2019/6/3
  * Time : 15:36
  */
-$chinaName ='最长公共前缀';
-$englishName = "longestCommonPrefix";
-$no = 14;
+$chinaName ='排列硬币';
+$englishName = "arrangeCoins";
+$no = 441;
 $str = $chinaName.'|'.$no.'|[' .$englishName.' url](https://github.com/huangxiaoxi0606/leetCode3/blob/master/'.$englishName.'/'.$englishName.'.php) | ['.$englishName.' url_test](https://github.com/huangxiaoxi0606/leetCode3/blob/master/'.$englishName.'/test.php)';
 file_put_contents('./leetCode.md', PHP_EOL.$str, FILE_APPEND);
